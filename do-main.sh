5540ad02794bac609e3532ad45b9fd34aa069dff
RyuujiX <saputradenny712@gmail.com>
2020-12-30 09:03:19 +0700

    Enable Pie and 65,68,71Hz

--- do-main.sh
+++ do-main.sh
@@ -10,21 +10,21 @@ fi
 FolderUp="BrokenNucleus"
 spectrumFile="f.rc"
 TypeBuild="Stable"
-TypeBuildTag="AOSP"
+TypeBuildTag="Anu"
 getInfo ">> Building kernel . . . . <<"
 
 CompileKernel
-# CompileKernel "65"
-# CompileKernel "68"
-# CompileKernel "71"
+CompileKernel "65"
+CompileKernel "68"
+CompileKernel "71"
 # CompileKernel "72"
 
 FixPieWifi
 
 CompileKernel
-# CompileKernel "65"
-# CompileKernel "68"
-# CompileKernel "71"
+CompileKernel "65"
+CompileKernel "68"
+CompileKernel "71"
 # CompileKernel "72"
 
 tg_send_info "All $GetKernelName $BuilderKernel already uploaded to Gdrive :D"
