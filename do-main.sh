b11e66ad301a42235593e80953b4df6fe271a647
RyuujiX <saputradenny712@gmail.com>
2021-01-17 23:12:34 +0700

    Enable all variants
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- do-main.sh
+++ do-main.sh
@@ -14,17 +14,17 @@ TypeBuildTag="Anu"
 getInfo ">> Building kernel . . . . <<"
 
 CompileKernel
-# CompileKernel "65"
-# CompileKernel "68"
-# CompileKernel "71"
+CompileKernel "65"
+CompileKernel "68"
+CompileKernel "71"
 # CompileKernel "72"
 
-# FixPieWifi
+FixPieWifi
 
-# CompileKernel
-# CompileKernel "65"
-# CompileKernel "68"
-# CompileKernel "71"
+CompileKernel
+CompileKernel "65"
+CompileKernel "68"
+CompileKernel "71"
 # CompileKernel "72"
 
 tg_send_info "Semua $GetKernelName $BuilderKernel dah diaplot ke GD Tod :v"
