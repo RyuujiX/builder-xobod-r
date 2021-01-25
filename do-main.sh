a89bb95452d831ea660b7de90ddce8b581e75fe2
RyuujiX <saputradenny712@gmail.com>
2021-01-25 18:37:00 +0700

    update builder mystic

--- do-main.sh
+++ do-main.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 branch="lynx"
-BuilderKernel="00000"
+BuilderKernel="mystic"
 
 if [ "$BuilderKernel" != "clang" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] && [ "$BuilderKernel" != "storm" ] && [ "$BuilderKernel" != "mystic" ] ;then
     exit;
