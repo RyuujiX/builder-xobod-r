61821adc6a4903b6c5877c4c44d954ccb250c6f6
RyuujiX <saputradenny712@gmail.com>
2021-03-01 01:21:03 +0700

    Add Missing Chat ID Prefix
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- main.sh
+++ main.sh
@@ -184,7 +184,7 @@ tg_send_info(){
         -d "parse_mode=html" \
         -d text="$1"
     else
-        curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="-1407005109" \
+        curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="-1001407005109" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="$1"
