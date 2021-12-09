723f53361348b538c56a67d87c7c043aa47d8097
RyuujiX <saputradenny712@gmail.com>
2021-12-09 18:18:50 +0700

    Update CAF Tag to `2900` for 4.19
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- do-main.sh
+++ do-main.sh
@@ -16,7 +16,7 @@ TypeBuild="RELEASE"
 BuilderKernel="00000"
 
 if [ "$KranulVer" = "419" ];then
-CAFTAG="02700"
+CAFTAG="02900"
 fi
 
 if [ "$BuilderKernel" != "proton" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] && [ "$BuilderKernel" != "gcc12" ] && [ "$BuilderKernel" != "storm" ] && [ "$BuilderKernel" != "strix" ] && [ "$BuilderKernel" != "yuki" ] && [ "$BuilderKernel" != "sdclang" ] && [ "$BuilderKernel" != "aosp" ] && [ "$BuilderKernel" != "elas" ] && [ "$BuilderKernel" != "iris" ] && [ "$BuilderKernel" != "atom" ] && [ "$BuilderKernel" != "zyc" ];then
