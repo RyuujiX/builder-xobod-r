02457372401359e74b4c1cdfd5f46f59cfc3bcfd
RyuujiX <saputradenny712@gmail.com>
2023-05-21 05:45:55 +0700

    Remove useless command
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- main.sh
+++ main.sh
@@ -8,11 +8,7 @@ getInfoErr() {
 }
 update_file() {
     if [ ! -z "$1" ] && [ ! -z "$2" ] && [ ! -z "$3" ];then
-        ValOri="$(echo "$GetValue" | awk -F '\\=' '{print $2}')"
-        UpdateTo="$(echo "$2" | awk -F '\\=' '{print $2}')"
-        [ "$ValOri" != "$UpdateTo" ] && \
         sed -i "$4s/$1.*/$2/g" "$3"
-        [ ! -z "$(git status | grep "modified" )" ] && \
         git add "$3"
     fi
 }
